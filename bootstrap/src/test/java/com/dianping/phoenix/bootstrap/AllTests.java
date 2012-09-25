428221883bfc0d8646d99d0f4557a78db7010bcb
Frankie Wu <qmwu2000@gmail.com>
2012-09-25 16:54:47 +0800

    add project platform-kernel

---
+++ bootstrap/src/test/java/com/dianping/phoenix/bootstrap/AllTests.java
@@ -0,0 +1,13 @@
+package com.dianping.phoenix.bootstrap;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+
+})
+public class AllTests {
+
+}
