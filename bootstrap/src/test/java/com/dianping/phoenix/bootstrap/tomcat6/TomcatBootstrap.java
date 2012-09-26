222e7ae7c73cc7d01675403f39f6676b2015c7e7
marsqing <qing.gu@dianping.com>
2012-09-26 15:25:27 +0800

    support config from argument

--- bootstrap/src/test/java/com/dianping/phoenix/bootstrap/tomcat6/TomcatBootstrap.java
+++ bootstrap/src/test/java/com/dianping/phoenix/bootstrap/tomcat6/TomcatBootstrap.java
@@ -16,15 +16,20 @@ import org.apache.catalina.realm.MemoryRealm;
 import org.apache.catalina.session.StandardManager;
 import org.apache.catalina.startup.Embedded;
 
+import com.dianping.phoenix.bootstrap.Tomcat6WebappLoader;
+
 /**
  * Utility class for starting tomcat via api
+ * 
  * @author marsqing
- *
+ * 
  */
 public class TomcatBootstrap {
 
 	private Embedded container = null;
+	private Host localHost;
 	private CountDownLatch shutdownLatch = new CountDownLatch(1);
+	private String kernelWarRoot;
 
 	/**
 	 * The directory to create the Tomcat server configuration under.
@@ -35,15 +40,20 @@ public class TomcatBootstrap {
 
 	/**
 	 * 
-	 * @param catalinaHome directory which contains a children directory named conf which contains all files
-	 * from a standard tomcat installation's conf directory
-	 * @param port port to listen
-	 * @param webappConfig web apps to start
+	 * @param catalinaHome
+	 *            directory which contains a children directory named conf which
+	 *            contains all files from a standard tomcat installation's conf
+	 *            directory
+	 * @param port
+	 *            port to listen
+	 * @param webappConfig
+	 *            web apps to start
 	 */
-	public TomcatBootstrap(String catalinaHome, int port, List<TomcatWebappConfig> webappConfig) {
+	public TomcatBootstrap(String catalinaHome, int port, List<TomcatWebappConfig> webappConfig, String kernelWarRoot) {
 		this.catalinaHome = catalinaHome;
 		this.port = port;
 		this.webappConfig = webappConfig;
+		this.kernelWarRoot = kernelWarRoot;
 	}
 
 	public void startTomcat() throws Exception {
@@ -53,8 +63,10 @@ public class TomcatBootstrap {
 		container.setRealm(new MemoryRealm());
 
 		// create host
-		Host localHost = container.createHost("localHost", new File(".").getAbsolutePath());
-		prepareContext(localHost);
+		localHost = container.createHost("localHost", new File(".").getAbsolutePath());
+		for (TomcatWebappConfig config : webappConfig) {
+			addWebapp(config);
+		}
 
 		// create engine
 		Engine engine = container.createEngine();
@@ -86,26 +98,30 @@ public class TomcatBootstrap {
 		shutdownLatch.await();
 	}
 
-	private void prepareContext(Host localHost) throws MalformedURLException {
-
-		for (TomcatWebappConfig config : webappConfig) {
-			WebappLoader loader = new WebappLoader(this.getClass().getClassLoader());
-			if (config.getClassesDir() != null) {
-				for (String classDir : config.getClassesDir()) {
-					loader.addRepository(new File(classDir).toURI().toURL().toString());
-				}
+	/**
+	 * Add a web app
+	 * 
+	 * @param config
+	 * @throws MalformedURLException
+	 */
+	public void addWebapp(TomcatWebappConfig config) throws MalformedURLException {
+		Tomcat6WebappLoader loader = new Tomcat6WebappLoader(this.getClass().getClassLoader());
+		loader.setKernelWebapp(kernelWarRoot);
+		if (config.getClassesDir() != null) {
+			for (String classDir : config.getClassesDir()) {
+				loader.addRepository(new File(classDir).toURI().toURL().toString());
 			}
-
-			Context context = container.createContext("/" + config.getContextPath(), config.getWebappDir());
-			context.setLoader(loader);
-			// avoid write SESSIONS.ser to src/test/resources/
-			StandardManager manager = new StandardManager();
-			manager.setPathname(System.getProperty("java.io.tmpdir"));
-			context.setManager(manager);
-			context.setReloadable(true);
-
-			localHost.addChild(context);
 		}
+
+		Context context = container.createContext("/" + config.getContextPath(), config.getWebappDir());
+		context.setLoader(loader);
+		// avoid write SESSIONS.ser to src/test/resources/
+		StandardManager manager = new StandardManager();
+		manager.setPathname(System.getProperty("java.io.tmpdir"));
+		context.setManager(manager);
+		context.setReloadable(true);
+
+		localHost.addChild(context);
 	}
 
 	public void stopTomcat() throws Exception {
@@ -113,24 +129,39 @@ public class TomcatBootstrap {
 	}
 
 	public static void main(String[] args) throws Exception {
-		final String baseDir = "../";
-		
+		String phoenixProjectRoot = "../";
+		String kernelWarRoot = "/Users/marsqing/Projects/phoenix/kernel/target/kernel/";
+
+		if (args != null && args.length != 2) {
+			System.out.println("Usage: phoenixProjectRoot kernelWarRoot");
+			System.exit(1);
+			phoenixProjectRoot = args[0];
+			kernelWarRoot = args[1];
+		}
+
+		File phoenixBase = new File(phoenixProjectRoot);
+		System.out.println("Starting tomcat with phoenix project root dir " + phoenixBase.getAbsolutePath());
+		System.out.println("\t\t and kernel war root dir " + kernelWarRoot);
+
 		final TomcatWebappConfig config = new TomcatWebappConfig();
-		config.setClassesDir(new ArrayList<String>() {
-			{
-				add(baseDir + "/samples/target/classes");
-			}
-		});
+		final String extraClassesDir = phoenixProjectRoot + "/samples/target/classes";
+		if ((new File(extraClassesDir)).exists()) {
+			config.setClassesDir(new ArrayList<String>() {
+				{
+					add(extraClassesDir);
+				}
+			});
+		}
 		config.setContextPath("");
-		config.setWebappDir(baseDir + "/samples/src/test/resources");
-		
-		final TomcatBootstrap inst = new TomcatBootstrap(baseDir + "/bootstrap/src/test/resources/tomcat6", 8089,
+		config.setWebappDir(phoenixProjectRoot + "/samples/src/test/resources");
+
+		final TomcatBootstrap inst = new TomcatBootstrap(phoenixProjectRoot + "/bootstrap/src/test/resources/tomcat6", 8080,
 				new ArrayList<TomcatWebappConfig>() {
 					{
 						add(config);
 					}
-				});
-		
+				}, kernelWarRoot);
+
 		new Thread() {
 			public void run() {
 				try {
@@ -140,7 +171,7 @@ public class TomcatBootstrap {
 				}
 			}
 		}.start();
-		
+
 		System.in.read();
 		inst.stopTomcat();
 	}
