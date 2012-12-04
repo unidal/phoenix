31c0f70b858a3488e2d2a2a5b2de6ea7be6d9d91
marsqing <qing.gu@dianping.com>
2012-12-04 18:47:02 +0800

    add getLogWriter method

--- phoenix-agent/src/main/java/com/dianping/phoenix/agent/core/tx/TransactionManager.java
+++ phoenix-agent/src/main/java/com/dianping/phoenix/agent/core/tx/TransactionManager.java
@@ -3,11 +3,13 @@ package com.dianping.phoenix.agent.core.tx;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Writer;
 
 
 public interface TransactionManager {
 
 	OutputStream getLogOutputStream(TransactionId txId) throws IOException;
+	Writer getLogWriter(TransactionId txId) throws IOException;
 	Reader getLogReader(TransactionId txId, int offset) throws IOException;
 	Transaction loadTransaction(TransactionId txId);
 	void saveTransaction(Transaction tx) throws IOException;
