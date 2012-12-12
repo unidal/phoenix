0b657f0528e9e7a8698a1f77b3d0844a66f57167
marsqing <qing.gu@dianping.com>
2012-12-12 11:53:40 +0800

    refactor view logic to jsp

--- phoenix-agent/src/main/java/com/dianping/phoenix/agent/page/deploy/Payload.java
+++ phoenix-agent/src/main/java/com/dianping/phoenix/agent/page/deploy/Payload.java
@@ -12,7 +12,7 @@ public class Payload implements ActionPayload<AgentPage, Action> {
 	private AgentPage m_page;
 
 	@FieldMeta("op")
-	private Action m_action;
+	private Action m_action = Action.DEFAULT;
 
 	@FieldMeta("deployId")
 	private long m_deployId;
@@ -81,41 +81,37 @@ public class Payload implements ActionPayload<AgentPage, Action> {
 
 	@Override
 	public void validate(ActionContext<?> ctx) {
-		StringBuilder sb = new StringBuilder();
 		switch (m_action) {
 		case DEPLOY:
-			checkCommonArguments(ctx, sb);
+			checkCommonArguments(ctx);
 			if (StringUtils.isEmpty(StringUtils.trimAll(m_domain))) {
-				sb.append("domain can not be null,");
+				ctx.addError(new ErrorObject("domain.invalid,"));
 			}
 			if (StringUtils.isEmpty(StringUtils.trimAll(m_version))) {
-				sb.append("version can not be null,");
+				ctx.addError(new ErrorObject("version.invalid"));
 			}
 			break;
 
 		case DETACH:
-			checkCommonArguments(ctx, sb);
+			checkCommonArguments(ctx);
 			if (StringUtils.isEmpty(StringUtils.trimAll(m_domain))) {
-				sb.append("domain can not be empty");
+				ctx.addError(new ErrorObject("domain.invalid"));
 			}
 			break;
 
 		case STATUS:
 		case CANCEL:
 		case GETLOG:
-			checkCommonArguments(ctx, sb);
+			checkCommonArguments(ctx);
 			break;
 
 		}
 
-		if (sb.length() > 0) {
-			ctx.addError(new ErrorObject(sb.toString()));
-		}
 	}
 
-	private void checkCommonArguments(ActionContext<?> ctx, StringBuilder sb) {
+	private void checkCommonArguments(ActionContext<?> ctx) {
 		if (!validDeployId(m_deployId)) {
-			sb.append("deployId invalid");
+			ctx.addError(new ErrorObject("deployId.invalid"));
 		}
 	}
 
