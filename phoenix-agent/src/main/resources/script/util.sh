d4e13e4a4d25c6437dec705f244cc5c07ead4cda
marsqing <qing.gu@dianping.com>
2012-12-17 13:11:40 +0800

    add log_error method

--- phoenix-agent/src/main/resources/script/util.sh
+++ phoenix-agent/src/main/resources/script/util.sh
@@ -3,6 +3,10 @@ function log {
 	echo "[`date +'%m-%d %H:%M:%S'`] [INFO] $@"
 }
 
+function log_error {
+	echo "[`date +'%m-%d %H:%M:%S'`] [ERROR] $@"
+}
+
 function ensure_not_empty {
 	for var in "$@";do
 		IFS='=' read -ra KV <<< "$var"
