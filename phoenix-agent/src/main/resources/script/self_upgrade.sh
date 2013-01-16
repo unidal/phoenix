3d6ede26df6b16ac1e603e5568ced0d1fec4490e
marsqing <qing.gu@dianping.com>
2013-01-16 17:49:56 +0800

    small refactor and add tx_log_file argument

--- phoenix-agent/src/main/resources/script/self_upgrade.sh
+++ phoenix-agent/src/main/resources/script/self_upgrade.sh
@@ -5,21 +5,23 @@ set -u
 cd `dirname $0`
 source ./util.sh
 
+log "PID is $$"
+log "CMD is $0 $@"
+
 AGENT_WAR_TMP=/data/webapps/phoenix/phoenix_agent_war_tmp/
 AGENT_DIR=/data/webapps/phoenix/phoenix-agent
 
-agent_git_url=/Users/marsqing/Projects/tmp/tmp/repo
-agent_version=v2
-while getopts "g:v:" option;do
+while getopts "g:v:l:" option;do
 	case $option in
 			g)      agent_git_url=$OPTARG;;
 			v)      agent_version=$OPTARG;;
+			l)      tx_log_file=$OPTARG;;
 	esac
 done
 
-tmp_update_script=`mktemp /tmp/phoenix-agent-self-upgrade.sh.XXXXXX`
-cat util.sh >> $tmp_update_script
-cat agent_func2.sh >> $tmp_update_script
-cat update_agent.sh >> $tmp_update_script
-chmod +x $tmp_update_script
-$tmp_update_script -g $agent_git_url -v $agent_version -t $AGENT_WAR_TMP -a $AGENT_DIR &
+tmp_upgrade_script=`mktemp /tmp/phoenix-agent-self-upgrade.sh.XXXXXX`
+cat util.sh >> $tmp_upgrade_script
+cat agent_func2.sh >> $tmp_upgrade_script
+cat upgrade_agent.sh >> $tmp_upgrade_script
+chmod +x $tmp_upgrade_script
+$tmp_upgrade_script -g $agent_git_url -v $agent_version -l $tx_log_file -t $AGENT_WAR_TMP -a $AGENT_DIR &
