0c23c76f9b8e5adb4cc81736b4ef358d44a8f14e
marsqing <qing.gu@dianping.com>
2013-04-15 17:43:48 +0800

    add ruby file to generate f5 and urlrewrite config file

---
+++ phoenix-router/src/main/resources/ruby/f5_rules.rb
@@ -0,0 +1,31 @@
+require 'citrus'
+require 'pp'
+require 'set'
+require 'stringio'
+
+def parse_f5(stringio)
+  Citrus.load 'f5'
+  doc = File.read('/Users/marsqing/Downloads/WEB.MAIN.txt')
+  f5 = F5.parse(doc, :root => :whens)
+
+  target_pool_set = Set.new
+  f5_rule_str = StringIO.new
+  pool_str = StringIO.new
+
+  f5.value.zip(f5.pool_targets).each do |patterns, target_pool|
+    if patterns && patterns[0] && /^HTTP.*/!~target_pool && /^\d.*/!~target_pool
+      target_pool_set.add target_pool
+      f5_rule_str << "<f5-rule target-pool='#{target_pool}'>\n"
+      patterns.each do |p|
+        p = p.gsub(/\\\\/, "\\")
+        f5_rule_str << "\t<pattern>#{p}</pattern>\n"
+      end
+      f5_rule_str << "</f5-rule>\n"
+    end
+  end
+
+  target_pool_set.each { |p| pool_str << "<!--pool name='#{p}' url-pattern='http://HOST:PORT/CONTEXT%s' /-->\n" }
+
+  stringio << pool_str.string
+  stringio << f5_rule_str.string
+end
