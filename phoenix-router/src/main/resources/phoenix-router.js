5b42e888a08c3fbb45c1521cd4af8826853f7380
marsqing <qing.gu@dianping.com>
2013-04-18 18:06:08 +0800

    use DIAO method

--- phoenix-router/src/main/resources/phoenix-router.js
+++ phoenix-router/src/main/resources/phoenix-router.js
@@ -1,29 +1,33 @@
 pathRegex = new RegExp("[^:]+://[^/]+(.*)");
+hostRegex = new RegExp("[^:]+://([^/]+).*");
+var body = document.body; 
+
 function pathOf(href) {
 	path = href.replace(pathRegex, "$1");
 	return path == "" ? "/" : path;
 }
 
 function shouldReplace(href) {
-	return true;
+	loc = window.location
+	curHost = loc.host;
+	aHost = href.replace(hostRegex, "$1");
+	return curHost != aHost;
 }
 
 function newHrefFor(oldHref) {
 	loc = window.location
-	newHref = loc.protocol + "//" + "www.dianping.com";
-	if(loc.port) {
-		newHref = newHref + ":" + loc.port;
-	}
+	newHref = loc.protocol + "//" + loc.host;
 	newHref = newHref + pathOf(oldHref);
 	return newHref;
 }
 
-var as = document.getElementsByTagName("a");
-// window.location == window.location.protocol + "//" + window.location.host + window.location.pathname
-for(i=0; i<as.length; i++) {
-	a = as[i];
-	href = a["href"];
-	if(shouldReplace()) {
-		a["href"] = newHrefFor(a["href"]);
-	}
-}
+body.addEventListener && body.addEventListener('click', function(e){
+    e.preventDefault();
+    if(e.target.tagName.toLowerCase() === 'a'){
+        var href = e.target.href;
+        if(shouldReplace(href)){
+        	href = newHrefFor(href);
+        }
+        location.href = href;
+    }
+}, false);
