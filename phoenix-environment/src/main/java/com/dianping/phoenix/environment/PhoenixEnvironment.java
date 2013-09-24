df1baf9e4963bcc90c3858538050e6424b37631b
atellwu <atell@qq.com>
2013-09-24 17:07:30 +0800

    添加注释

--- phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironment.java
+++ phoenix-environment/src/main/java/com/dianping/phoenix/environment/PhoenixEnvironment.java
@@ -3,6 +3,12 @@ package com.dianping.phoenix.environment;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 基于ThreadLocal的通用内存容器，用于在各个平台级别的组件中传递变量
+ * 
+ * @author kezhu.wu
+ * 
+ */
 public class PhoenixEnvironment {
 
     public static final String                      MOBILE_REQUEST_ID       = "pragma-page-id";
@@ -25,14 +31,33 @@ public class PhoenixEnvironment {
                                                                                 }
                                                                             };
 
+    /**
+     * 根据key获取存在ThreadLocal的map中的对象
+     * 
+     * @param key
+     * @return
+     */
     public static Object get(String key) {
         return get(key, false);
     }
 
+    /**
+     * 以key将value存在ThreadLocal的map中
+     * 
+     * @param key
+     * @return
+     */
     public static void set(String key, Object value) {
         set(key, value, false);
     }
 
+    /**
+     * 以key将value存在ThreadLocal的map中
+     * 
+     * @param key
+     * @param inheritable 如果为true，则从InheritableThreadLocal的map获取，否则从ThreadLocal的map获取
+     * @return
+     */
     public static Object get(String key, boolean inheritable) {
         if (inheritable) {
             return inheritableMap.get().get(key);
@@ -41,6 +66,13 @@ public class PhoenixEnvironment {
         }
     }
 
+    /**
+     * 以key将value存在ThreadLocal中
+     * 
+     * @param key
+     * @param inheritable 如果在true，则使用InheritableThreadLocal的map来存储，否则使用ThreadLocal的map存储
+     * @return
+     */
     public static void set(String key, Object value, boolean inheritable) {
         if (inheritable) {
             inheritableMap.get().put(key, value);
@@ -49,6 +81,9 @@ public class PhoenixEnvironment {
         }
     }
 
+    /**
+     * 获取metas，用于页头插入到html中
+     */
     public static String getMetas() {
         return (String) get(METAS);
     }
