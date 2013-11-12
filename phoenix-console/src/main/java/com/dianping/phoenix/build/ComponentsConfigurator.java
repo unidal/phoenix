6d15e30c4bb1c9f0223123d2c466c13345e5f592
songtong <tong.song@dianping.com>
2013-11-12 18:37:48 +0800

    bug fix

--- phoenix-console/src/main/java/com/dianping/phoenix/build/ComponentsConfigurator.java
+++ phoenix-console/src/main/java/com/dianping/phoenix/build/ComponentsConfigurator.java
@@ -34,7 +34,6 @@ import com.dianping.phoenix.service.LogService;
 import com.dianping.phoenix.service.ProjectManager;
 import com.dianping.phoenix.service.WarService;
 import com.dianping.phoenix.service.resource.DefaultResourceManager;
-import com.dianping.phoenix.service.resource.MockResourceManager;
 import com.dianping.phoenix.service.resource.ResourceManager;
 import com.dianping.phoenix.service.resource.cmdb.DefaultDeviceManager;
 import com.dianping.phoenix.service.resource.cmdb.DeviceManager;
@@ -83,7 +82,7 @@ public class ComponentsConfigurator extends AbstractResourceConfigurator {
 				.req(ConfigManager.class));
 		all.add(C(ProjectManager.class, DefaultProjectManager.class) //
 				.req(DeploymentDao.class, DeploymentDetailsDao.class));
-		all.add(C(ResourceManager.class, MockResourceManager.class)//
+		all.add(C(ResourceManager.class, DefaultResourceManager.class)//
 				.req(DeviceManager.class, ConfigManager.class, AgentStatusFetcher.class));
 
 		for (DeployPolicy policy : DeployPolicy.values()) {
